0c05ddd8bd92c2e2900df9f923ec3f6c585a6fb1
sanjit <sanjit@sanjits-MacBook-Pro.local>
2022-07-21 21:53:22 +0530

    some changes

--- src/components/SearchHome.js
+++ src/components/SearchHome.js
@@ -16,7 +16,7 @@ class SearchHome extends Component{
 
     }
     componentDidMount() {
-        this.getPostDataResult()
+
     }
 
     handleChange = (e) => {
@@ -68,51 +68,27 @@ class SearchHome extends Component{
         console.log('search_text', p_id)
         return(
             <div className="container p-5">
-                {loading &&
-                <div className="d-flex justify-content-center form-box">
-                    <div className="spinner-grow text-danger" role="status" style={{width:'60px', height:'60px'}}>
-                        <span className="visually-hidden">Loading...</span>
-                    </div>
-                </div>
-                }
-                {/*<div className="row">*/}
-                    {/*<div className="col-xl-6">*/}
-                        {/*<div className="shadow p-3 mb-5 bg-body rounded">*/}
-                            {/*<h2 className="text-center">Search Your Keyword</h2>*/}
-                            {/*<div className="input-group mb-3 mt-4">*/}
-                                {/*<input type="text" className="form-control" placeholder="Type keyword here..."*/}
-                                       {/*aria-label="Recipient's username" aria-describedby="basic-addon2"*/}
-                                {/*onChange={(e)=> {this.handleChange(e)}}*/}
-                                       {/*onKeyDown={(e)=> {this.handleChange(e)}}*/}
-                                {/*/>*/}
-                                {/*<button className="btn btn-primary"*/}
-                                        {/*onClick={() => {*/}
-                                            {/*this.props.history.push({*/}
-                                                {/*pathname: `${`/search-result`}/`,*/}
-                                                {/*state: {post_id: p_id}*/}
-                                            {/*})*/}
-                                        {/*}}*/}
-                                {/*>Search</button>*/}
-                            {/*</div>*/}
+                <div className="row form-box">
+                    <div className="col-xl-6 m-auto">
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <h2 className="text-center">Search Your Keyword</h2>
+                            <div className="input-group mb-3 mt-4">
+                                <input type="text" className="form-control" placeholder="Type keyword here..."
+                                       aria-label="Recipient's username" aria-describedby="basic-addon2"
+                                onChange={(e)=> {this.handleChange(e)}}
+                                       onKeyDown={(e)=> {this.handleChange(e)}}
+                                />
+                                <button className="btn btn-primary"
+                                        onClick={() => {
+                                            this.props.history.push({
+                                                pathname: `${`/search-result`}/`,
+                                                state: {post_id: p_id}
+                                            })
+                                        }}
+                                >Search</button>
+                            </div>
 
-                        {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
-                <div className="row">
-                    <div className="col-xl-10 m-auto">
-                        {postList.map((singalPost, i)=>{
-                            const {id, userId, body, title} = singalPost
-                            return(
-                                <div className="card shadow mb-3" key={id} style={{cursor:'pointer'}}>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{title.charAt(0).toUpperCase() + title.slice(1)}</h5>
-                                        <p className="card-text">{body}</p>
-                                        <a href="#" className="card-link">See Post</a>
-                                    </div>
-                                </div>
-
-                            )
-                        })}
+                        </div>
                     </div>
                 </div>
             </div>
